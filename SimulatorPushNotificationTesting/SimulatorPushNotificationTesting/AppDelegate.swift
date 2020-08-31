3cc5c48d4338a68f35c124af9757219905f338d8
prakashmaharjan <prakashmaharjan20@gmail.com>
2020-08-31 16:48:51 +0545

    added to git

---
+++ SimulatorPushNotificationTesting/SimulatorPushNotificationTesting/AppDelegate.swift
@@ -0,0 +1,79 @@
+//
+//  AppDelegate.swift
+//  SimulatorPushNotificationTesting
+//
+//  Created by prakash Maharjan on 8/31/20.
+//  Copyright © 2020 pm. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        // push notification request
+        let notificationCenter = UNUserNotificationCenter.current()
+        notificationCenter.delegate = self
+        notificationCenter.requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+            // handle permissions here
+            if granted == true && error == nil {
+                print ("push granted")
+            }
+            
+            
+        }
+        return true
+    }
+    
+
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+       
+       let response =  response.notification.request.content.userInfo
+       print("response is : \(response)")
+    }
+    
+    
+    
+   func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+    
+      completionHandler([.alert, .badge, .sound])
+        
+
+    }
+    
+    
+    
+    
+    
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+           print("Push register failed error: \(error.localizedDescription)")
+           
+       }
+       
+       func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+           print("Push device token is: \(deviceToken)")
+       }
+
+    
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
